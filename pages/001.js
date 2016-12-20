638edf977d4cf158ebcb824da6ef750b5a117b04
MartinC <chadcho82@hotmail.com>
2016-12-21 10:55:48 +1300

    Commit

---
+++ pages/001.js
@@ -0,0 +1,15 @@
+pageComponentry = {
+  data: function() {
+    return {
+      // Any page specific data goes here.
+    }
+  },
+  methods: {
+    // Any page specific methods go here.
+  },
+  ready: function() {
+    console.log('weffwfwefwewefewffewfewf');
+    // Ready will be fired when the page is loaded.
+    courseFeatureJBA.transitionIn();
+  }
+}
