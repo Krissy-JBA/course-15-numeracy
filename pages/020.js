b1d25f52883efa277275744d3316783638a4c4ae
MartinC <chadcho82@hotmail.com>
2016-12-22 13:08:50 +1300

    Page 19 - 23

---
+++ pages/020.js
@@ -0,0 +1,15 @@
+pageComponentry = {
+  data: function() {
+    return {
+      // Any page specific data goes here.
+    }
+  },
+  methods: {
+    // Any page specific methods go here.
+  },
+  ready: function() {
+    // Ready will be fired when the page is loaded.
+    courseFeatureJBA.transitionIn();
+    courseFeatureJBA.activateDataPopups();
+  }
+}
