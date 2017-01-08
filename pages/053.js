d683a38c8f0ba6e44a277a6b38fb2a2fc412d753
MartinC <chadcho82@hotmail.com>
2017-01-09 12:57:17 +1300

    Create all pages name and built pages until 67

---
+++ pages/053.js
@@ -0,0 +1,57 @@
+pageComponentry = {
+  data: function() {
+    return {
+      // Any data goes here.
+      pageData: {
+        download: ''
+      },
+
+      disabledInput: true,
+      hideLocked: false
+
+    }
+  },
+  methods: {
+    validateInputFields: function(){
+      if(!this.pageData.download){
+        this.hideLocked = false,
+        this.disabledInput = true
+      } else {
+        this.hideLocked = true,
+        this.disabledInput = false
+      }
+    },
+
+   callPageTransition: function(){
+      var poppedElement = $(".pop-in");
+        $(poppedElement).each(function() {
+            var thisPop = this;
+            setInterval(function() {
+                $(thisPop).addClass("popOut");
+            }, 200);
+        });
+        setTimeout(function(){
+          window.location.href = "#054";
+        },1000)
+    },
+
+    sendData: function(){
+      if(this.disabledInput){
+        return;
+      } else {
+        this.callPageTransition();
+      }
+    },
+  },
+  ready: function() {
+    //call transition
+    courseFeatureJBA.transitionIn();
+    courseFeatureJBA.activateTextBoxUi();
+    courseFeatureJBA.activateDataPopups();
+
+    //load data on page load
+    this.pageData.standFor = this.exerciseData['your-numeracy-skill.download'];
+    //populate data before calling the validate function
+    this.validateInputFields();
+  }
+}
---
+++ pages/064.js
@@ -0,0 +1,59 @@
+pageComponentry = {
+  data: function() {
+    return {
+      pageData: {
+        knowledge: '',
+        wisdom: ''
+      },
+
+      hideLocked: false,
+      disabledInput: true
+
+    }
+  },
+  methods: {
+    callPageTransition: function(){
+      var poppedElement = $(".pop-in");
+        $(poppedElement).each(function() {
+            var thisPop = this;
+            setInterval(function() {
+                $(thisPop).addClass("popOut");
+            }, 200);
+        });
+        setTimeout(function(){
+          window.location.href = "#065";
+        },1000)
+    },
+
+    validateInputFields: function(){
+      if(!this.pageData.knowledge || !this.pageData.wisdom){
+        this.hideLocked = false,
+        this.disabledInput = true
+      } else {
+        this.hideLocked = true,
+        this.disabledInput = false
+      }
+    },
+
+    sendData: function(){
+      var obj = this.pageData;
+      if(this.disabledInput){
+        return;
+      } else {
+        this.callPageTransition();
+      }
+    }
+
+  },
+  ready: function() {
+    courseFeatureJBA.transitionIn();
+    courseFeatureJBA.activateTextBoxUi();
+
+    //load data on page load
+    this.pageData.knowledge = this.exerciseData['activity-time.knowledge'];
+    this.pageData.wisdom = this.exerciseData['activity-time.wisdom'];
+
+    //populate data before calling the validate function
+    this.validateInputFields();
+  }
+}
