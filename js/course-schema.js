8879df30cb89e253e7082cb6dc710c930fc0ab4c
MartinC <chadcho82@hotmail.com>
2017-01-24 11:26:50 +1300

    Course schema and change name of module's title

--- js/course-schema.js
+++ js/course-schema.js
@@ -1,3 +1,20 @@
 courseSchema = {
-
+  modules: [
+    {
+        'name': 'Before course start',
+        'first_page': '001'
+    },{
+        'name': 'Intro to numeracy',
+        'first_page': '018'
+    },{
+        'name': 'Confidence in solving issues',
+        'first_page': '037'
+    },{
+        'name': 'Strategies for improving numeracy',
+        'first_page': '079'
+    },{
+        'name': 'Numeracy in the business world',
+        'first_page': '087'
+    }
+  ]
 }
